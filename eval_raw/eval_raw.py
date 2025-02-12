3d21397f073cbcaded1f7739259710a9e73096af
Jesse Liu <112784299+jesseliu0913@users.noreply.github.com>
2025-02-12 13:42:49 -0600

    Update eval_raw.py

--- eval_raw/eval_raw.py
+++ eval_raw/eval_raw.py
@@ -118,26 +118,13 @@ output_name = type_name if type_name != "none" else args.data
 output_name = output_name if "/" not in output_name else output_name.split("/")[-1]
 
 with open(
-    f"/mnt/deepseek/eval_raw/resutls/raw/mmlu/{output_name}.json", "w"
+    f"/mnt/deepseekv2/eval_raw/resutls/raw/mmlu/{output_name}.json", "w"
 ) as fw:
     json.dump(full_expert_dict, fw, indent=4)
 
 
 
 """
-CUDA_VISIBLE_DEVICES=4 nohup python eval_raw_model.py nyu-mll/glue ax test 2 --sub_one premise --sub_two hypothesis >> ./log/raw/glue.ax 2>&1 &
-CUDA_VISIBLE_DEVICES=5 nohup python eval_raw_model.py nyu-mll/glue cola test 1 --sub_one sentence >> ./log/raw/glue.cola 2>&1 &
-CUDA_VISIBLE_DEVICES=6 nohup python eval_raw_model.py nyu-mll/glue mnli_matched test 2 --sub_one premise --sub_two hypothesis >> ./log/raw/glue.mnli_matched 2>&1 &
-CUDA_VISIBLE_DEVICES=7 nohup python eval_raw_model.py nyu-mll/glue mnli_mismatched test 2 --sub_one premise --sub_two hypothesis >> ./log/raw/glue.mnli_mismatched 2>&1 &
-CUDA_VISIBLE_DEVICES=2 nohup python eval_raw_model.py nyu-mll/glue mrpc test 2 --sub_one sentence1 --sub_two sentence2 >> ./log/raw/glue.mrpc 2>&1 &
-CUDA_VISIBLE_DEVICES=3 nohup python eval_raw_model.py nyu-mll/glue qnli test 2 --sub_one question --sub_two sentence >> /scratch0/zx22/zijie/llama-moe_lzj/new_cluster/log/raw/glue.qnli 2>&1 &
-# CUDA_VISIBLE_DEVICES=6 nohup python eval_raw_model.py nyu-mll/glue qqp test 2 --sub_one question1 --sub_two question2 >> /scratch0/zx22/zijie/llama-moe_lzj/new_cluster/log/raw/glue.qqp 2>&1 &
-CUDA_VISIBLE_DEVICES=6 nohup python eval_raw_model.py nyu-mll/glue rte test 2 --sub_one sentence1 --sub_two sentence2 >> /scratch0/zx22/zijie/llama-moe_lzj/new_cluster/log/raw/glue.rte 2>&1 &
-CUDA_VISIBLE_DEVICES=7 nohup python eval_raw_model.py nyu-mll/glue sst2 test 1 --sub_one sentence >> /scratch0/zx22/zijie/llama-moe_lzj/new_cluster/log/raw/glue.sst2 2>&1 &
-CUDA_VISIBLE_DEVICES=7 nohup python eval_raw_model.py nyu-mll/glue stsb test 2 --sub_one sentence1 --sub_two sentence2 >> /scratch0/zx22/zijie/llama-moe_lzj/new_cluster/log/raw/glue.stsb 2>&1 &
-CUDA_VISIBLE_DEVICES=6 nohup python eval_raw_model.py nyu-mll/glue wnli test 2 --sub_one sentence1 --sub_two sentence2 >> /scratch0/zx22/zijie/llama-moe_lzj/new_cluster/log/raw/glue.wnli 2>&1 &
-
-
 CUDA_VISIBLE_DEVICES=4 nohup python eval_raw_model.py piqa none test 3 --sub_one goal --sub_two sol1 --sub_three sol2 > ./log/raw/piqa.lb 2>&1 &
 CUDA_VISIBLE_DEVICES=5 nohup python eval_raw_model.py winogrande winogrande_debiased test 3 --sub_one sentence --sub_two option1 --sub_three option2 > ./log/raw/winogrande_debiased.lb 2>&1 &
 CUDA_VISIBLE_DEVICES=6 nohup python eval_raw_model.py Rowan/hellaswag none test 3 --sub_one ctx_a --sub_two ctx_b --sub_three activity_label > ./log/raw/hellaswag.lb 2>&1 &
@@ -145,6 +132,4 @@ CUDA_VISIBLE_DEVICES=7 nohup python eval_raw_model.py truthful_qa generation val
 CUDA_VISIBLE_DEVICES=1 nohup python eval_raw_model.py truthful_qa multiple_choice validation 3 --sub_one question --sub_two mc1_targets --sub_three mc2_targets > ./log/raw/multiple_choice.lb 2>&1 &
 CUDA_VISIBLE_DEVICES=6 nohup python eval_raw_model.py gsm8k main test 2 --sub_one question --sub_two answer > ./log/raw/main.lb 2>&1 &
 CUDA_VISIBLE_DEVICES=7 nohup python eval_raw_model.py gsm8k socratic test 2 --sub_one question --sub_two answer > ./log/raw/socratic.lb 2>&1 &
-
-
 """
