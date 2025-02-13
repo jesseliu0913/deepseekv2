9ecb193fba45e1894fec08f2339687dba105a90e
Jesse Liu <112784299+jesseliu0913@users.noreply.github.com>
2025-02-13 04:39:50 +0000

    Update cal_lb.py

--- eval_raw/cal_lb.py
+++ eval_raw/cal_lb.py
@@ -11,9 +11,10 @@ def get_expert(file_data, expert_num=64):
     max_expert_lst = []
     layer_full_lst = [[] for idx in range(26)]
     length = int(len(list(file_data.keys())) * 0.1 )
-    # print("length is", len(list(file_data.keys())[0:length]))
+    print("length is", len(list(file_data.keys())[0:length]))
     for key in list(file_data.keys())[0:length]:
         token_info = file_data[key]
+        print(len(token_info))
         for layer_index, layer_info in enumerate(token_info):
             layer_info = flatten_2d_list(layer_info)
             # if layer_index != 26:
