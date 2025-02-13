5fc1aa80f38b73dd0ee825e64b595ac323cf2dd4
Jesse Liu <112784299+jesseliu0913@users.noreply.github.com>
2025-02-12 19:44:48 -0600

    Create cal_lb.py

---
+++ eval_raw/cal_lb.py
@@ -0,0 +1,44 @@
+import os
+import numpy as np
+import json
+
+
+def flatten_2d_list(twd_list):
+    return [element for od_list in twd_list for element in od_list]
+
+def get_expert(file_data, expert_num=64):
+    layer_gap_dict = {}
+    max_expert_lst = []
+    layer_full_lst = [[] for idx in range(27)]
+
+    for key in list(file_data.keys()):
+        token_info = file_data[key]
+        for layer_index, layer_info in enumerate(token_info):
+            layer_info = flatten_2d_list(layer_info)
+            layer_full_lst[layer_index].extend(layer_info)
+
+
+    for layer_index, layer_info in enumerate(layer_full_lst):
+        assert all(not isinstance(item, list) for item in layer_info) == True
+        expert_quant = layer_info
+        sample_nums = len(expert_quant)
+        average_expert = sample_nums / expert_num
+        expert_count_list = [expert_quant.count(i) for i in range(expert_num)]
+
+        max_expert = np.argmax(expert_count_list)
+        max_expert_tokens = expert_count_list[max_expert]
+        max_expert_lst.append(max_expert)
+        # print(max_expert_tokens)
+        # print(average_expert)
+        gap = max_expert_tokens / average_expert
+
+        layer_gap_dict[layer_index] = gap
+    return layer_gap_dict
+
+folder_path = "./results"
+files = os.listdir(folder_path)
+print(files)
+for file in files:
+  json_data = json.load(open(os.path.join(folder_path, file), "r"))
+  layer_gap_dict = get_expert(json_data, expert_num=64)
+  print(np.means(np.array(list(layer_gap_dict.values))))
