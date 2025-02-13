f0f417f8647a8d17270f4c62b7dab602fd00e4d2
Jesse Liu <112784299+jesseliu0913@users.noreply.github.com>
2025-02-12 23:56:15 -0600

    Update analysis_results.py

--- cal_important/analysis_results.py
+++ cal_important/analysis_results.py
@@ -11,8 +11,7 @@ for file in files:
   print(file)
   score_lst_sep = []
   file_data = json.load(open(os.path.join(folder_path, file), "r"))
-  print(file_data['26'])
-  print(len(list(file_data.keys())))
+
   for key in list(file_data.keys()):
     score_dict[int(key)] += np.mean(np.array(file_data[key]), axis=1)
     score_lst_sep.append(np.argmax(np.mean(np.array(file_data[key]), axis=1)))
